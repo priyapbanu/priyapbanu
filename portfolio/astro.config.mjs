b62642b6aefc85d781485288e6dc01fae14bdd4c
priyapbanu <priyapbanu2@gmail.com>
2026-03-01 16:48:13 +0530

    fix deployment configs

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://priyapbanu.github.io',
-  // base: '/', // Uncomment and set to '/repo-name' if not using root domain
   vite: {
     plugins: [tailwindcss()],
   },
